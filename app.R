6ffec42ebb0848a6380f24ad29c156824d8962a9
Karim Amri <146330254+doriankari@users.noreply.github.com>
2024-01-04 20:32:40 +0100

    bg

--- app.R
+++ app.R
@@ -1,53 +1,125 @@
+# # # Simulation de données de criminalité à Los Angeles
 source("packages.R")
-source("global.R")
+#source("global.R")
 
 # UI
-ui <- fluidPage(
-  titlePanel("Crimes à Los Angeles de 2020 à 2023"),
+ui <- dashboardPage(
+  dashboardHeader(title = "Dashboard CRIME LA"),
+  dashboardSidebar(
+    sidebarMenu(
+      pickerInput(
+        "city_select", label = "Villes",
+        choices = unique(data$AREA.NAME),
+        options = list('actions-box' = TRUE),
+        multiple = TRUE,
+        selected = unique(data$AREA.NAME)[1:4]
+      ),
+      pickerInput(
+        "crime_select", label = "Crimes",
+        choices = c("Cambriolage" = "BURGLARY", 
+                    "Viol" = "RAPE, FORCIBLE", 
+                    "Vol" = "ROBBERY", 
+                    "Enlèvement" = "KIDNAPPING", 
+                    "Véhicule volé" = "VEHICLE - STOLEN"),
+        options = list('actions-box' = TRUE),
+        multiple = TRUE,
+        selected = c("BURGLARY", "ROBBERY")
+      ),
+
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Map", tabName = "map", icon = icon("map")),
+      menuItem("Graphs", tabName = "graphique", icon = icon("chart-line")),
+      menuItem("Data Set", tabName = "data_set", icon = icon("table")),
+      menuItem("Summary", tabName = "Summary", icon = icon("chart-pie"))
+    )
+  ),
   
-  sidebarLayout(
-    sidebarPanel(
-      # Vous pouvez ajouter des filtres supplémentaires ici
-      # par exemple : selectInput(), dateRangeInput(), etc.
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Graphique", 
-                 plotOutput("crimePlot")
-        ),
-        tabPanel("Carte", 
-                 leafletOutput("crimeMap")
+  
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "home",
+        h2("USA, Los Angeles Crimes Data: 2020 To 2023", align = "center"),
+        br(),
+        p("This application offers an in-depth exploration of crime data in Los Angeles,
+  providing a comprehensive analysis of trends, variations, and criminal 
+  patterns prevalent within this metropolis.
+  Its aim is to deliver a nuanced and contextualized understanding of 
+  the city's criminal landscape through interactive visualizations and 
+  data analysis tools.", align = "center"),
+        br(),
+        div(
+          style = "text-align: center;",
+          tags$img(src = "LAPD.png", height = "400px", width = "600px"),
+          br(),
+          br(),
+          p(em("Done by Mr Amri Karim."),br(),em("contact: amri.dk@hotmail.com"))
         )
+      ),
+      tabItem(
+        tabName = "map",
+        leafletOutput("map")
+      ),
+      tabItem(
+        tabName = "graphique",
+        tabsetPanel(
+          tabPanel("Armes Utilisées", icon = icon("gun")),
+          tabPanel("Sexe des Victimes", icon = icon("venus-mars")),
+          tabPanel("Ethnicité des Victimes", icon = icon("globe-americas")),
+          tabPanel("Type de Crime", icon = icon("exclamation-triangle"))
+        )
+        #plotOutput("graphique_output")
+      ),
+      tabItem(
+        tabName = "data_set",
+        dataTableOutput("tableau"),
+      ),
+      tabItem(
+        tabName = "Summary",
+        verbatimTextOutput("SummaryData")
       )
     )
   )
 )
-
+#
 # Server logic
 server <- function(input, output) {
   
-  # Fonction pour générer le graphique
-  output$crimePlot <- renderPlot({
-    # Vous pouvez utiliser ggplot pour créer un graphique en fonction des données
-    # Ceci est un exemple générique. Assurez-vous d'adapter votre code à vos données.
-    ggplot(data, aes(x = Date, fill = Crm.Cd.Desc)) +
-      geom_bar() +
-      labs(title = "Nombre de crimes par type",
-           x = "Date",
-           y = "Nombre de crimes") +
-      theme_minimal()
+  # Création d'une variable réactive pour filtrer les données en fonction des sélections de ville et de crime
+  filtered_data <- reactive({
+    selected_cities <- input$city_select
+    selected_crimes <- input$crime_select
+    
+    # Filtrer les données en fonction des sélections
+    filtered <- data[data$AREA.NAME %in% selected_cities & data$Crm.Cd.Desc %in% selected_crimes, ]
+    return(filtered)
   })
   
-  # Fonction pour générer la carte
-  output$crimeMap <- renderLeaflet({
-    # Vous pouvez utiliser Leaflet pour créer une carte interactive
-    # Ceci est un exemple générique. Assurez-vous d'adapter votre code à vos données.
-    leaflet(data) %>%
+  output$map <- renderLeaflet({
+    leaflet(filtered_data()) %>%
       addTiles() %>%
-      addMarkers(~34.052235, ~118.243683, popup = ~Crm.Cd.Desc)
+      addCircleMarkers(clusterOptions = markerClusterOptions(
+        radius = 5, # Taille du marqueur
+        color = "blue", # Couleur du marqueur
+        fillOpacity = 0.8, # Opacité du remplissage
+        popup = ~filtered_data()$Crm.Cd.Desc, # Texte du popup
+        label = ~filtered_data()$Crm.Cd.Desc # Texte affiché lorsque vous survolez le marqueur
+      ))
+  })
+  
+  output$graphique_output <- renderPlot({
+    # Code pour créer vos graphiques en fonction des sélections (à compléter)
+    # Utilisez les données filtrées : filtered_data()
+  })
+  
+  output$tableau <- renderDataTable({
+    filtered_data()
+  })
+  
+  output$SummaryData <- renderPrint({
+    summary(filtered_data())
   })
 }
 
-# Lancer l'application Shiny
+
 shinyApp(ui = ui, server = server)
--- global.r
+++ global.r
@@ -1,10 +1,28 @@
-# Importe les données 
+# Import
 
 print("data charged")
-data <- read.csv("DATA/Crime_Data.csv", header = TRUE, sep = ";")
+
+data <- read.csv("DATA/CRIME_DATA_LA.csv", header = TRUE, sep = ";")
+
+data <- data %>%
+  filter(LAT >= 24.396308 & LAT <= 49.384358 & LON >= -125.000000 & LON <= -66.934570)
+
+data_pop <- data %>%
+  filter(data$vict.sex %in% c("F", "M","X"))
+
+data_ethnie <- data_pop %>%
+  group_by(area.name, vict.descent, vict.sex) %>%
+  mutate( vict.descent = case_when(
+    vict.descent == "W" ~ "White",
+    vict.descent == "B" ~ "Black",
+    vict.descent == "H" ~ "Hispanic",
+    TRUE ~ "Other"
+  ),
+  vict.sex = case_when(
+    vict.sex == "F" ~ "Ladies",
+    vict.sex == "M" ~ "Mens",
+    TRUE ~ "Other"
+  )) %>%
+  summarise(nbre_victime = n())
+
 print("data load")
-
-titre_appli = " analyse crime 2023"
-
-variables_data = names(data)
-
